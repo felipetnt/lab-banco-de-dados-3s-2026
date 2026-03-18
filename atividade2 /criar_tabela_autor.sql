37c0b28c2e991c0daa99ba9e9118c848ee9b565f
felipetnt <felipeashlei@gmail.com>
2026-03-18 10:20:45 -0300

    Create autor table with relevant fields

---
+++ atividade2 /criar_tabela_autor.sql
@@ -0,0 +1,9 @@
+CREATE TABLE autor (
+    id_autor INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255) NOT NULL UNIQUE,
+    ano_nascimento YEAR,
+    ano_morte YEAR,
+    apresentacao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
